8034e81133b36572182d9557855201ad064a9a11
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-11-05 11:15:35 -0500

    add first part of passport local config

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// passport configuration
+require('./config/passport')(passport);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
