ebfad8e3e019edac7fd2b828dbc8a68021d7d07a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-11-05 10:16:17 -0500

    added mongodb connection

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+// Registering npm modules
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+//additions for authentication
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var passport = require('passport');
+
+//DB Setup 
+var DB = require('./config/db.js');
+mongoose.connect(DB.url);
+mongoose.connection.on('error', function(){
+  console.error('MongoDB Connection Error');
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
